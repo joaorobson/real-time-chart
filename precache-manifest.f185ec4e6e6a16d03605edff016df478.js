9c1196d5890744270c25a2d5459e6cdfb18a680a
joao robson <joaorobsonmartins@gmail.com>
2019-06-18 11:49:28 -0300

    Updates

---
+++ precache-manifest.f185ec4e6e6a16d03605edff016df478.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "035dc4019f19752c21ed29b4a7a206fd",
+    "url": "/real-time-chart/index.html"
+  },
+  {
+    "revision": "ea95efe541321511b02e",
+    "url": "/real-time-chart/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c0b72b9118e46d986b50",
+    "url": "/real-time-chart/static/js/2.642b459c.chunk.js"
+  },
+  {
+    "revision": "ea95efe541321511b02e",
+    "url": "/real-time-chart/static/js/main.a783fffb.chunk.js"
+  },
+  {
+    "revision": "50bc6770bc74743a1a38",
+    "url": "/real-time-chart/static/js/runtime~main.2d08ada3.js"
+  }
+]);
